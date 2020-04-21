9ba10ab0f3b6f4ff33f878bab33a81c321280638
Yuki Adachi <yuki777@gmail.com>
2020-04-21 11:07:19 +0700

    Icon and Image

--- send.sh
+++ send.sh
@@ -2,7 +2,17 @@
 source .env
 curl \
         -s \
-        -d "{ \"to\": \"/topics/${TOPIC_NAME}\", \"priority\": \"high\", \"notification\": { \"title\": \"titleタイトル\", \"body\": \"bodyボディ\", \"click_action\": \"${CLICK_ACTION}\" } }" \
+        -d "{ \
+        \"to\": \"/topics/${TOPIC_NAME}\", \
+        \"priority\": \"high\", \
+        \"notification\": { \
+            \"title\": \"テストTitle\", \
+            \"body\": \"テストBody\", \
+            \"icon\": \"https://via.placeholder.com/60x60.png?text=TEST\", \
+            \"image\": \"https://via.placeholder.com/480x360.png?text=TEST\", \
+            \"click_action\": \"${CLICK_ACTION}\" \
+        } \
+        }" \
         -H "Authorization: key=${FIREBASE_SERVER_KEY}" \
         -H "Content-type: application/json" \
         https://fcm.googleapis.com/fcm/send
